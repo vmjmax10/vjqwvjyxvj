89ad2c91aaf4e4d2bba2d1ff270450e106aaaa55
Vijay <sjain2787@gmail.com>
2022-01-05 04:30:46 +0530

    updated

--- exps/yolox_s_forms_persp.py
+++ exps/yolox_s_forms_persp.py
@@ -11,22 +11,22 @@ class Exp(MyExp):
         super(Exp, self).__init__()
         
         # ---------------- model config ---------------- #
-        self.num_classes = 1
-        self.depth = 0.67
-        self.width = 0.75
+        self.num_classes = 5
+        self.depth = 0.33
+        self.width = 0.50
 
         # ---------------- dataloader config ---------------- #
         # set worker to 4 for shorter dataloader init time
         self.data_num_workers = 4
-        self.input_size = (1024, 1024)  # (height, width)
+        self.input_size = (640, 640)  # (height, width)
         self.multiscale_range = 0
         # self.random_size = (14, 26)
-        self.data_dir = "/content/vjqwvjyxvj/gdrive/MyDrive/DATASET/WORD_DETECTION_DATASET_DICTS/YOLOX"
+        self.data_dir = "/content/vjqwvjyxvj/gdrive/MyDrive/DATASET/FORMS_PERSP_DATASET_DICTS/YOLOX"
         self.train_ann = "train_coco.json"
         self.val_ann = "val_coco.json"
         self.train_img_dir = "train"
         self.val_img_dir = "val"
-        self.output_dir = "/content/vjqwvjyxvj/gdrive/MyDrive/TRAINED_MODELS/WORD_DETECTION_DATASET_DICTS"
+        self.output_dir = "/content/vjqwvjyxvj/gdrive/MyDrive/TRAINED_MODELS/FORMS_PERSP_DATASET_DICTS"
         
         # --------------- transform config ----------------- #
         self.mosaic_prob = 2.0
@@ -43,26 +43,26 @@ class Exp(MyExp):
 
         # --------------  training config --------------------- #
         self.warmup_epochs = 5
-        self.max_epoch = 1000
+        self.max_epoch = 200
         self.warmup_lr = 0
         self.basic_lr_per_img = 0.01 / 64.0
         self.scheduler = "yoloxwarmcos"
         self.no_aug_epochs = 15
         self.min_lr_ratio = 0.05
         self.ema = True
-        self.max_labels_tt = 500
-        self.max_labels_mosaicd = 1000
+        self.max_labels_tt = 5
+        self.max_labels_mosaicd = 5
         self.flip_image = False
 
         self.weight_decay = 5e-4
         self.momentum = 0.9
         self.print_interval = 25
         
-        self.eval_interval = 10
+        self.eval_interval = 5
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         # self.exp_name = "yolox_s_vjs_fp16"
 
         # -----------------  testing config ------------------ #
-        self.test_size = (1024, 1024)
+        self.test_size = (640, 640)
         self.test_conf = 0.01
         self.nmsthre = 0.25
