c6fba49c646179316b14dc578a99e9d03c85a1f4
vijay <sjain2787@gmail.com>
2022-04-19 14:37:01 +0530

    updated

--- exps/yolox_s_word_det.py
+++ exps/yolox_s_word_det.py
@@ -25,7 +25,7 @@ class Exp(MyExp):
         self.train_ann = "train_coco.json"
         self.val_ann = "val_coco.json"
         self.train_img_dir = "train"
-        self.val_img_dir = "val"
+        self.val_img_dir = "train"
         self.output_dir = "/content/vjqwvjyxvj/gdrive/MyDrive/TRAINED_MODELS/LAYOUT_WORD_DS"
         
         # --------------- transform config ----------------- #
@@ -42,27 +42,27 @@ class Exp(MyExp):
         self.enable_mixup = True
 
         # --------------  training config --------------------- #
-        self.warmup_epochs = 30
-        self.max_epoch = 500
+        self.warmup_epochs = 50
+        self.max_epoch = 300
         self.warmup_lr = 0
         self.basic_lr_per_img = 0.01 / 64.0
         self.scheduler = "yoloxwarmcos"
-        self.no_aug_epochs = 30
+        self.no_aug_epochs = 300
         self.min_lr_ratio = 0.05
         self.ema = True
         self.max_labels_tt = 1000
-        self.max_labels_mosaicd = 1500
+        self.max_labels_mosaicd = 1000
         self.flip_image = False
 
         self.weight_decay = 5e-4
         self.momentum = 0.9
         self.print_interval = 120
         
-        self.eval_interval = 3
+        self.eval_interval = 5
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         # self.exp_name = "yolox_s_vjs_fp16"
 
         # -----------------  testing config ------------------ #
         self.test_size = self.input_size
         self.test_conf = 0.01
-        self.nmsthre = 0.20
+        self.nmsthre = 0.25
