6506ad7bf365e8b10085782637295c2b7aa4cae5
vijay <sjain2787@gmail.com>
2022-07-13 18:48:50 +0530

    updated

--- exps/yolox_s_word_det.py
+++ exps/yolox_s_word_det.py
@@ -19,7 +19,7 @@ class Exp(MyExp):
         # set worker to 4 for shorter dataloader init time
         self.data_num_workers = 4
         self.input_size = (2048, 2048)  # (height, width)
-        self.multiscale_range = 10
+        self.multiscale_range = 2
         # self.random_size = (14, 26)
         self.data_dir = "/content/vjqwvjyxvj/gdrive/MyDrive/DATASET/LAYOUT_WORD_DS/YOLOX"
         self.train_ann = "train_coco.json"
@@ -42,12 +42,12 @@ class Exp(MyExp):
         self.enable_mixup = False
 
         # --------------  training config --------------------- #
-        self.warmup_epochs = 30
-        self.max_epoch = 150
+        self.warmup_epochs = 10
+        self.max_epoch = 200
         self.warmup_lr = 0
-        self.basic_lr_per_img = 0.01 / 64.0
+        self.basic_lr_per_img = 0.01 / 16.0
         self.scheduler = "yoloxwarmcos"
-        self.no_aug_epochs = 150
+        self.no_aug_epochs = 200
         self.min_lr_ratio = 0.05
         self.ema = True
         self.max_labels_tt = 1000
